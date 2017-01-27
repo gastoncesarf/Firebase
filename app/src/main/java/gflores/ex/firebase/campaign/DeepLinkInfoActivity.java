26a34a10f8d8b66da5ab290be10367a6e01d5384
gastoncesarf <gastoncesarf@gmail.com>
2017-01-27 14:24:55 -0300

    Refactor

--- app/src/main/java/gflores/ex/firebase/campaign/DeepLinkInfoActivity.java
+++ app/src/main/java/gflores/ex/firebase/campaign/DeepLinkInfoActivity.java
@@ -1,4 +1,13 @@
 package gflores.ex.firebase.campaign;
 
-public class DeepLinkInfoActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DeepLinkInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deep_link_info);
+    }
 }
--- app/src/main/java/gflores/ex/firebase/campaign/MainActivity.java
+++ app/src/main/java/gflores/ex/firebase/campaign/MainActivity.java
@@ -1,10 +1,14 @@
 package gflores.ex.firebase.campaign;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import com.google.android.gms.appinvite.AppInvite;
 import com.google.android.gms.appinvite.AppInviteInvitationResult;
 import com.google.android.gms.appinvite.AppInviteReferral;
@@ -21,27 +25,36 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.O
 
         // Build GoogleApiClient with AppInvite API for receiving deep links
         final GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
-            .enableAutoManage(this, this)
-            .addApi(AppInvite.API)
-            .build();
+                .enableAutoManage(this, this)
+                .addApi(AppInvite.API)
+                .build();
+
+        final TextView deepLinkData = (TextView) findViewById(R.id.deep_link_info_label);
+        final Button deepLinkButton = (Button) findViewById(R.id.deep_link_button);
 
         // Check if this app was launched from a deep link. Setting autoLaunchDeepLink to true
         // would automatically launch the deep link if one is found.
         final boolean autoLaunchDeepLink = false;
         AppInvite.AppInviteApi.getInvitation(mGoogleApiClient, this, autoLaunchDeepLink)
-            .setResultCallback(
-                new ResultCallback<AppInviteInvitationResult>() {
-                    @Override
-                    public void onResult(@NonNull AppInviteInvitationResult result) {
-                        if (result.getStatus().isSuccess()) {
-                            // Extract deep link from Intent
-                            final Intent intent = result.getInvitationIntent();
-                            final String deepLink = AppInviteReferral.getDeepLink(intent);
-                        } else {
-                            Log.d("AppInvite", "getInvitation: no deep link found.");
-                        }
-                    }
-                });
+                .setResultCallback(
+                        new ResultCallback<AppInviteInvitationResult>() {
+                            @Override
+                            public void onResult(@NonNull AppInviteInvitationResult result) {
+                                if (result.getStatus().isSuccess()) {
+                                    // Extract deep link from Intent
+                                    final Intent intent = result.getInvitationIntent();
+                                    deepLinkData.setText(AppInviteReferral.getDeepLink(intent));
+                                    deepLinkButton.setOnClickListener(new View.OnClickListener() {
+                                        @Override
+                                        public void onClick(View v) {
+                                            startActivity(intent);
+                                        }
+                                    });
+                                } else {
+                                    Log.d("AppInvite", "getInvitation: no deep link found.");
+                                }
+                            }
+                        });
     }
 
     @Override
